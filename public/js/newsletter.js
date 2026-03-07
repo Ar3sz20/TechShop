3c2edf003ffd781529c9728b5bfa51ea1a88dc33
Levi <236532819+Z-Levente@users.noreply.github.com>
2026-03-07 18:52:00 +0100

    newsletter fix

--- public/js/newsletter.js
+++ public/js/newsletter.js
@@ -2,10 +2,16 @@ const model = document.querySelector('.model');
 const modelCloseBtn = document.querySelector('.model-close-btn');
 const modelCloseOverlay = document.querySelector('.model-close-overlay');
 
-if(model && modelCloseBtn && modelCloseOverlay){
+if (localStorage.getItem("modalClosed") === "true") {
+    if (model) model.classList.add("closed");
+}
+
+if (model && modelCloseBtn && modelCloseOverlay) {
 
     const modelCloseFunc = () => {
         model.classList.add('closed');
+        localStorage.setItem("modalClosed", "true");
+
     };
 
     modelCloseOverlay.addEventListener('click', modelCloseFunc);
@@ -16,8 +22,12 @@ if(model && modelCloseBtn && modelCloseOverlay){
 //Ha nem kell tényleges feliratkozás!
 const newsletterForm = document.querySelector(".newsletter form");
 
-newsletterForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    document.querySelector(".model").classList.add("closed");
-    document.querySelector(".overlay").classList.remove("active");
-});
+if (newsletterForm) {
+    newsletterForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        document.querySelector(".model").classList.add("closed");
+        localStorage.setItem("modalClosed", "true");
+        document.querySelector(".overlay").classList.remove("active");
+
+    })
+};
