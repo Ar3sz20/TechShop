33e2583880799743c613bffffcf03f8dc15299e5
HDave <119301666+Ar3sz20@users.noreply.github.com>
2026-03-12 11:34:09 +0100

    little repair and storeproductrequest

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -11,7 +11,7 @@ class StoreProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,13 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => ["required","string", "max: 50"],
+            "type" => ["required","string", "max:30"],
+            "price" => ["required","decimal" , "min: 0.99", "max: 9999999999999.99"],
+            "category" => ["required","string" , "max:30"],
+            "description" => ["required","string", "max:255"],
+            "image" => ["required","image","mimes:jpeg,png,jpg,gif,webp","max:16000"],
+            "quantity" => ["required","integer", "min: 0"]
         ];
     }
 }
